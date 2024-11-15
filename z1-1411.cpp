af2e4a05cfd3945742e38eeab47cd1eaf423de1d
ValentinaVidovic <valentina.vidovic@sum.ba>
2024-11-15 16:26:03 +0100

    Add files via upload

---
+++ z1-1411.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream> // Uključivanje knjižnice za ulazno/izlazne operacije (unos i ispis)
+using namespace std; // Korištenje standardnog imenskog prostora za olakšavanje upotrebe objekata iz standardne knjižnice
+
+float a, b; // Definiranje dviju varijabli 'a' i 'b' tipa float (decimalni brojevi) za unos brojeva
+
+int main() // Glavna funkcija programa
+{
+    //cout << "Unesite prvi broj: "; // Komentarirani redak, bio je korišten za ispis poruke prije unosa prvog broja
+    //cin >> a; // Komentarirani redak, omogućava unos prvog broja i pohranu u varijablu 'a'
+    //cout << "Unesite drugi broj: "; // Komentarirani redak, bio je korišten za ispis poruke prije unosa drugog broja
+    //cin >> b; // Komentarirani redak, omogućava unos drugog broja i pohranu u varijablu 'b'
+
+    cout << "Unesite dva broja: "; // Ispis poruke na ekranu za unos dva broja
+    cin >> a >> b; // Unos dva broja od korisnika koji se pohranjuju u varijable 'a' i 'b'
+
+    cout << "Zbroj unesenih brojeva je " << a + b << endl; // Ispis zbroja dvaju brojeva
+    cout << "Razlika unesenih brojeva je " << a - b << endl; // Ispis razlike između dvaju brojeva
+    cout << "Umnozak unesenih brojeva je " << a * b << endl; // Ispis umnoška dvaju brojeva
+    cout << "Količnik unesenih brojeva je " << a / b << endl; // Ispis količnika dvaju brojeva
+
+    return 0; // Završetak funkcije main, vraća 0 kao signal da je program uspješno završio
+}
---
+++ z2-1411.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream> // Uključivanje knjižnice za ulazno/izlazne operacije (unos i ispis)
+using namespace std; // Korištenje standardnog imenskog prostora za olakšavanje upotrebe objekata iz standardne knjižnice
+
+int ocjena; // Definiranje varijable 'ocjena' koja će pohraniti unesenu ocjenu
+
+int main() // Glavna funkcija programa
+{
+    cout << "Unesite ocjenu "; // Ispis poruke korisniku da unese svoju ocjenu
+    cin >> ocjena; // Unos ocjene koju korisnik unosi, a vrijednost se pohranjuje u varijablu 'ocjena'
+
+    switch (ocjena) { // Započinje switch-case strukturu koja provodi uvjetno grananje na temelju unesene ocjene
+    case 1: // Ako je unesena ocjena 1
+        cout << "Nazalost niste polozili ispit. Pokusajte ponovno!"; // Ispisuje poruku za ocjenu 1
+        break; // Prekid switch-case blokova, završava daljnje provjere
+    case 2: // Ako je unesena ocjena 2
+        cout << "Cestitam polozili ste ocjenom 2."; // Ispisuje poruku za ocjenu 2
+        break; // Prekid switch-case blokova
+    case 3: // Ako je unesena ocjena 3
+        cout << "Cestitam polozili ste ocjenom 3."; // Ispisuje poruku za ocjenu 3
+        break; // Prekid switch-case blokova
+    case 4: // Ako je unesena ocjena 4
+        cout << "Cestitam polozili ste ocjenom 4."; // Ispisuje poruku za ocjenu 4
+        break; // Prekid switch-case blokova
+    case 5: // Ako je unesena ocjena 5
+        cout << "Cestitam polozili ste ocjenom 5."; // Ispisuje poruku za ocjenu 5
+        break; // Prekid switch-case blokova
+    default: // Ako unesena ocjena nije ni 1, 2, 3, 4, niti 5 (neispravan unos)
+        cout << "Pogresan unos !"; // Ispisuje poruku u slučaju neispravnog unosa
+        break; // Prekid switch-case blokova
+    }
+    return 0; // Završetak glavne funkcije i program završava s kodom 0
+}
